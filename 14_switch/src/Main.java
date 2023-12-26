cda7ff3f3b80381769959f7fcda02150b8982a52
khaled___beno <khaled___beno@localhost>
2023-12-26 19:24:46 +0100

    Switch

---
+++ 14_switch/src/Main.java
@@ -0,0 +1,55 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		int var = 2;
+	
+		
+		
+		switch (var) {
+		
+		case 1:
+			System.out.println("La variable es 1");
+			break;
+			
+		case 2:
+			System.out.println("La variable contiene 2");
+			break;
+			
+		case 3:
+			System.out.println("La variable contiene 3");
+			break;
+			
+			default:
+				System.out.println("No corresponde a ningun caso");
+				break;
+			
+		}
+		
+		String cadena = "C";
+		
+	switch (var) {
+		
+		case 1:
+			System.out.println("La variable es C");
+			break;
+			
+		case 2:
+			System.out.println("La variable contiene A");
+			break;
+			
+		case 3:
+			System.out.println("La variable contiene B");
+			break;
+			
+			default:
+				System.out.println("No corresponde a ningun caso");
+				break;
+	}
+		
+	}
+
+}
